4da8a32e65a66f7c8080b59f6f8d8a9978fbde35
Tomas Balaz <balaztomas0@gmail.com>
2024-01-09 13:10:43 +0100

    Adjust the Consumer for receiving Json message

--- src/main/java/sk/balaz/kafka/consumer/KafkaConsumer.java
+++ src/main/java/sk/balaz/kafka/consumer/KafkaConsumer.java
@@ -3,6 +3,7 @@ package sk.balaz.kafka.consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import sk.balaz.kafka.payload.Student;
 
 import static java.lang.String.format;
 
@@ -10,8 +11,23 @@ import static java.lang.String.format;
 @Slf4j
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "test", groupId = "consumer-group")
+//    @KafkaListener(topics = "test", groupId = "consumer-group")
+//    curl --location 'http://localhost:8080/api/v1/messages' \
+//            --header 'Content-Type: text/plain' \
+//            --data 'This my first message'
     public void consumeMessage(String message) {
         log.info( format("Consuming the message from test topic:: %s", message));
+    }
+
+    @KafkaListener(topics = "test", groupId = "consumer-group")
+//    curl --location 'http://localhost:8080/api/v1/messages/json' \
+//            --header 'Content-Type: application/json' \
+//            --data '{
+//            "id" : 3,
+//            "firstName" : "marion",
+//            "lastName" : "12345"
+//}'
+    public void consumeJsonMessage(Student student) {
+        log.info( format("Consuming the student from test topic:: %s", student));
     }
 }
