cea74f9759677dbe215202697ea43f23702eea04
Tomas Balaz <balaztomas0@gmail.com>
2024-01-09 10:57:58 +0100

    Create a Kafka Producer

---
+++ src/main/java/sk/balaz/kafka/producer/KafkaProducer.java
@@ -0,0 +1,21 @@
+package sk.balaz.kafka.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        log.info(format("Sending message to test topic:: %s", message));
+        kafkaTemplate.send("test", message);
+    }
+}
